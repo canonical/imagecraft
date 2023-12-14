200ac0aebb0373a4d4800ca2271b473c2776a73f
Paul Mars <paul.mars@canonical.com>
2023-12-14 09:42:08 +0100

    Test cli.py

---
+++ tests/unit/test_cli.py
@@ -0,0 +1,28 @@
+# This file is part of imagecraft.
+#
+# Copyright 2023 Canonical Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+from unittest.mock import call
+
+from craft_cli import emit
+from imagecraft import cli
+
+
+def test_run_init(mocker):
+    mock_ended_ok = mocker.spy(emit, "ended_ok")
+    mocker.patch.object(sys, "argv", ["imagecraft", "version"])
+    cli.run()
+    assert mock_ended_ok.mock_calls == [call()]
