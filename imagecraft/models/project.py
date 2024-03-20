3f1e68914d3010de9b0a4193802d52d1762f1093
Paul Mars <paul.mars@canonical.com>
2024-03-20 09:44:21 +0100

    Cleaning

--- imagecraft/models/project.py
+++ imagecraft/models/project.py
@@ -19,12 +19,13 @@
 This module defines a imagecraft.yaml file, exportable to a JSON schema.
 """
 
-from collections.abc import Iterable, Mapping
+from collections.abc import Mapping
 from typing import TYPE_CHECKING, Any
 
 import pydantic
 from craft_application.models import BuildInfo
 from craft_application.models import Project as BaseProject
+from craft_application.util.error_formatting import format_pydantic_errors
 
 # pyright: reportMissingTypeStubs=false
 from craft_archives.repo.package_repository import (  # type: ignore[import-untyped]
@@ -48,12 +49,13 @@ if TYPE_CHECKING:
 else:
     UniqueStrList = conlist(str, unique_items=True, min_items=1)
 
+file_name = "imagecraft.yaml"
 
 def _alias_generator(s: str) -> str:
     return s.replace("_", "-")
 
 class ProjectValidationError(CraftError):
-    """Error validatiing image.yaml."""
+    """Error validating imagecraft.yaml."""
 
 class ProjectModel(YamlModelMixin, BaseProject):
     """Base model for the imagecraft project class."""
@@ -161,7 +163,7 @@ class Project(ProjectModel):
             return cls.parse_obj({**data})
         except ValidationError as error:
             raise ProjectValidationError(
-                _format_pydantic_errors(error.errors()),
+                format_pydantic_errors(error.errors(), file_name=file_name),
             ) from error
 
 
@@ -189,93 +191,3 @@ class Project(ProjectModel):
                 )
 
         return build_infos
-
-
-def _format_pydantic_errors(errors: list[Any], *, file_name: str = "imagecraft.yaml") -> str:
-    """Format errors.
-
-    Example 1: Single error.
-
-    Bad iamgecraft.yaml content:
-    - field: <some field>
-      reason: <some reason>
-
-    Example 2: Multiple errors.
-
-    Bad imagecraft.yaml content:
-    - field: <some field>
-      reason: <some reason>
-    - field: <some field 2>
-      reason: <some reason 2>
-    """
-    combined = [f"Bad {file_name} content:"]
-    for error in errors:
-        formatted_loc = _format_pydantic_error_location(error["loc"])
-        formatted_msg = _format_pydantic_error_message(error["msg"])
-
-        if formatted_msg == "field required":
-            field_name, location = _printable_field_location_split(formatted_loc)
-            combined.append(
-                f"- field {field_name} required in {location} configuration",
-            )
-        elif formatted_msg == "extra fields not permitted":
-            field_name, location = _printable_field_location_split(formatted_loc)
-            combined.append(
-                f"- extra field {field_name} not permitted in {location} configuration",
-            )
-        elif formatted_msg == "the list has duplicated items":
-            field_name, location = _printable_field_location_split(formatted_loc)
-            combined.append(
-                f" - duplicate entries in {field_name} not permitted in {location} configuration",
-            )
-        elif formatted_loc == "__root__":
-            combined.append(f"- {formatted_msg}")
-        else:
-            combined.append(f"- {formatted_msg} (in field {formatted_loc!r})")
-
-    return "\n".join(combined)
-
-def _format_pydantic_error_location(loc: Iterable[str | int]) -> str:
-    """Format location."""
-    loc_parts: list[str] = []
-    for loc_part in loc:
-        if isinstance(loc_part, str):
-            loc_parts.append(loc_part)
-        elif isinstance(loc_part, int): # pyright: ignore[reportUnnecessaryIsInstance]
-            # Integer indicates an index. Go
-            # back and fix up previous part.
-            previous_part = loc_parts.pop()
-            previous_part += f"[{loc_part}]"
-            loc_parts.append(previous_part)
-        else:
-            raise TypeError(f"unhandled loc: {loc_part}")
-
-    loc = ".".join(loc_parts)
-
-    # Filter out internal __root__ detail.
-    return loc.replace(".__root__", "")
-
-
-def _format_pydantic_error_message(msg: str) -> str:
-    """Format pydantic's error message field."""
-    # Replace shorthand "str" with "string".
-    return msg.replace("str type expected", "string type expected")
-
-def _printable_field_location_split(location: str) -> tuple[str, str]:
-    """Return split field location.
-
-    If top-level, location is returned as unquoted "top-level".
-    If not top-level, location is returned as quoted location, e.g.
-
-    (1) field1[idx].foo => 'foo', 'field1[idx]'
-    (2) field2 => 'field2', top-level
-
-    :returns: tuple of <field name>, <location> as printable representations.
-    """
-    loc_split = location.split(".")
-    field_name = repr(loc_split.pop())
-
-    if loc_split:
-        return field_name, repr(".".join(loc_split))
-
-    return field_name, "top-level"
