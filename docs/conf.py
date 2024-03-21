9f95ca1c51075194c770a139d7ba8c9b411ace6d
Robert Krátký <rkratky@gmail.com>
2024-03-21 15:12:16 +0100

    Initial docs set up for RTD.

--- docs/conf.py
+++ docs/conf.py
@@ -1,174 +1,172 @@
+import sys
+import os
+
+sys.path.append("./")
+from custom_conf import *
+from build_requirements import *
+
 # Configuration file for the Sphinx documentation builder.
+# You should not do any modifications to this file. Put your custom
+# configuration into the custom_conf.py file.
+# If you need to change this file, contribute the changes upstream.
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-project = "Imagecraft"
-copyright = "2023, Canonical"
-author = "Canonical Ltd."
-
-# Update with the favicon for your product
-# html_favicon = "_static/favicon.png"
-
-html_context = {
-    # Change to the link to your product website (without "https://")
-    "product_page": "github.com/canonical/imagecraft",
-    # Add your product tag to ".sphinx/_static" and change the path
-    # here (start with "_static"), default is the circle of friends
-    "product_tag": "_static/tag.png",
-    # Change to the discourse instance you want to be able to link to
-    # (use an empty value if you don't want to link)
-    # "discourse": "https://discourse.ubuntu.com/c/rocks/117",
-    # Change to the GitHub info for your project
-    "github_url": "https://github.com/canonical/imagecraft",
-    # Change to the branch for this version of the documentation
-    "github_version": "main",
-    # Change to the folder that contains the documentation (usually "/" or "/docs/")
-    "github_folder": "/docs/",
-    # Change to an empty value if your GitHub repo doesn't have issues enabled
-    "github_issues": "enabled",
-}
-
-
-if "discourse" in html_context:
-    html_context["discourse_prefix"] = html_context["discourse"] + "/t/"
-
-# region General configuration
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+############################################################
+### Extensions
+############################################################
 
 extensions = [
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.viewcode",
-    "sphinx.ext.coverage",
-    "sphinx.ext.doctest",
     "sphinx_design",
     "sphinx_copybutton",
-    "sphinx-pydantic",
-    # "sphinx_toolbox.more_autodoc",
-    "sphinx.ext.autodoc",  # Must be loaded after more_autodoc
+    "sphinxcontrib.jquery",
 ]
 
+# Only add redirects extension if any redirects are specified.
+if AreRedirectsDefined():
+    extensions.append("sphinx_reredirects")
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
+# Only add myst extensions if any configuration is present.
+if IsMyStParserUsed():
+    extensions.append("myst_parser")
+
+    # Additional MyST syntax
+    myst_enable_extensions = ["substitution", "deflist", "linkify"]
+    myst_enable_extensions.extend(custom_myst_extensions)
+
+# Only add Open Graph extension if any configuration is present.
+if IsOpenGraphConfigured():
+    extensions.append("sphinxext.opengraph")
+
+extensions.extend(custom_extensions)
+extensions = DeduplicateExtensions(extensions)
+
+### Configuration for extensions
+
+# Used for related links
+if not "discourse_prefix" in html_context and "discourse" in html_context:
+    html_context["discourse_prefix"] = html_context["discourse"] + "/t/"
+
+# The URL prefix for the notfound extension depends on whether the documentation uses versions.
+# For documentation on documentation.ubuntu.com, we also must add the slug.
+url_version = ""
+url_lang = ""
+
+# Determine if the URL uses versions and language
+if (
+    "READTHEDOCS_CANONICAL_URL" in os.environ
+    and os.environ["READTHEDOCS_CANONICAL_URL"]
+):
+    url_parts = os.environ["READTHEDOCS_CANONICAL_URL"].split("/")
+
+    if (
+        len(url_parts) >= 2
+        and "READTHEDOCS_VERSION" in os.environ
+        and os.environ["READTHEDOCS_VERSION"] == url_parts[-2]
+    ):
+        url_version = url_parts[-2] + "/"
+
+    if (
+        len(url_parts) >= 3
+        and "READTHEDOCS_LANGUAGE" in os.environ
+        and os.environ["READTHEDOCS_LANGUAGE"] == url_parts[-3]
+    ):
+        url_lang = url_parts[-3] + "/"
+
+# Set notfound_urls_prefix to the slug (if defined) and the version/language affix
+if slug:
+    notfound_urls_prefix = "/" + slug + "/" + url_lang + url_version
+elif len(url_lang + url_version) > 0:
+    notfound_urls_prefix = "/" + url_lang + url_version
+else:
+    notfound_urls_prefix = ""
+
+notfound_context = {
+    "title": "Page not found",
+    "body": "<p><strong>Sorry, but the documentation page that you are looking for was not found.</strong></p>\n\n<p>Documentation changes over time, and pages are moved around. We try to redirect you to the updated content where possible, but unfortunately, that didn't work this time (maybe because the content you were looking for does not exist in this version of the documentation).</p>\n<p>You can try to use the navigation to locate the content you're looking for, or search for a similar page.</p>\n",
+}
+
+# Default image for OGP (to prevent font errors, see
+# https://github.com/canonical/sphinx-docs-starter-pack/pull/54 )
+if not "ogp_image" in locals():
+    ogp_image = "https://assets.ubuntu.com/v1/253da317-image-document-ubuntudocs.svg"
+
+############################################################
+### General configuration
+############################################################
+
 exclude_patterns = [
     "_build",
     "Thumbs.db",
     ".DS_Store",
-    "env",
-    "sphinx-starter-pack",
-    "common",
+    ".sphinx",
 ]
+exclude_patterns.extend(custom_excludes)
 
-templates_path = ["_templates"]
+rst_epilog = """
+.. include:: /reuse/links.txt
+"""
+if "custom_rst_epilog" in locals():
+    rst_epilog = custom_rst_epilog
 
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}
 
-show_authors = False
+if not "conf_py_path" in html_context and "github_folder" in html_context:
+    html_context["conf_py_path"] = html_context["github_folder"]
 
-# endregion
-# region Options for HTML output
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+# For ignoring specific links
+linkcheck_anchors_ignore_for_url = [r"https://github\.com/.*"]
+linkcheck_anchors_ignore_for_url.extend(custom_linkcheck_anchors_ignore_for_url)
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
+# Tags cannot be added directly in custom_conf.py, so add them here
+for tag in custom_tags:
+    tags.add(tag)  # type: ignore
+
+############################################################
+### Styling
+############################################################
+
+# Find the current builder
+builder = "dirhtml"
+if "-b" in sys.argv:
+    builder = sys.argv[sys.argv.index("-b") + 1]
+
+# Setting templates_path for epub makes the build fail
+if builder == "dirhtml" or builder == "html":
+    templates_path = [".sphinx/_templates"]
+    notfound_template = "404.html"
+
+# Theme configuration
 html_theme = "furo"
 html_last_updated_fmt = ""
 html_permalinks_icon = "¶"
-html_theme_options = {
-    "light_css_variables": {
-        "color-sidebar-background-border": "none",
-        "font-stack": "Ubuntu, -apple-system, Segoe UI, Roboto, Oxygen, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif",
-        "font-stack--monospace": "Ubuntu Mono, Consolas, Monaco, Courier, monospace",
-        "color-foreground-primary": "#111",
-        "color-foreground-secondary": "var(--color-foreground-primary)",
-        "color-foreground-muted": "#333",
-        "color-background-secondary": "#FFF",
-        "color-background-hover": "#f2f2f2",
-        "color-brand-primary": "#111",
-        "color-brand-content": "#06C",
-        "color-api-background": "#cdcdcd",
-        "color-inline-code-background": "rgba(0,0,0,.03)",
-        "color-sidebar-link-text": "#111",
-        "color-sidebar-item-background--current": "#ebebeb",
-        "color-sidebar-item-background--hover": "#f2f2f2",
-        "toc-font-size": "var(--font-size--small)",
-        "color-admonition-title-background--note": "var(--color-background-primary)",
-        "color-admonition-title-background--tip": "var(--color-background-primary)",
-        "color-admonition-title-background--important": "var(--color-background-primary)",
-        "color-admonition-title-background--caution": "var(--color-background-primary)",
-        "color-admonition-title--note": "#24598F",
-        "color-admonition-title--tip": "#24598F",
-        "color-admonition-title--important": "#C7162B",
-        "color-admonition-title--caution": "#F99B11",
-        "color-highlighted-background": "#EbEbEb",
-        "color-link-underline": "var(--color-background-primary)",
-        "color-link-underline--hover": "var(--color-background-primary)",
-        "color-version-popup": "#772953",
-    },
-    "dark_css_variables": {
-        "color-foreground-secondary": "var(--color-foreground-primary)",
-        "color-foreground-muted": "#CDCDCD",
-        "color-background-secondary": "var(--color-background-primary)",
-        "color-background-hover": "#666",
-        "color-brand-primary": "#fff",
-        "color-brand-content": "#06C",
-        "color-sidebar-link-text": "#f7f7f7",
-        "color-sidebar-item-background--current": "#666",
-        "color-sidebar-item-background--hover": "#333",
-        "color-admonition-background": "transparent",
-        "color-admonition-title-background--note": "var(--color-background-primary)",
-        "color-admonition-title-background--tip": "var(--color-background-primary)",
-        "color-admonition-title-background--important": "var(--color-background-primary)",
-        "color-admonition-title-background--caution": "var(--color-background-primary)",
-        "color-admonition-title--note": "#24598F",
-        "color-admonition-title--tip": "#24598F",
-        "color-admonition-title--important": "#C7162B",
-        "color-admonition-title--caution": "#F99B11",
-        "color-highlighted-background": "#666",
-        "color-link-underline": "var(--color-background-primary)",
-        "color-link-underline--hover": "var(--color-background-primary)",
-        "color-version-popup": "#F29879",
-    },
-}
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+if html_title == "":
+    html_theme_options = {"sidebar_hide_name": True}
 
-# These paths are either relative to html_static_path
-# or fully qualified paths (eg. https://...)
+############################################################
+### Additional files
+############################################################
+
+html_static_path = [".sphinx/_static"]
+
 html_css_files = [
-    "css/custom.css",
-    "github_issue_links.css",
     "custom.css",
     "header.css",
+    "github_issue_links.css",
+    "furo_colors.css",
 ]
+html_css_files.extend(custom_html_css_files)
 
-# endregion
-# region Options for extensions
-# Intersphinx extension
-# https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html#configuration
-
-intersphinx_mapping = {
-    "python": ("https://docs.python.org/3", None),
-}
-
-# Type hints configuration
-set_type_checking_flag = True
-typehints_fully_qualified = False
-always_document_param_types = True
-typehints_document_rtype = True
-linkcheck_anchors_ignore = ["slice-definitions"]
-
-# Enable support for google-style instance attributes.
-napoleon_use_ivar = True
-
-# Github config
-github_username = "canonical"
-github_repository = "imagecraft"
+html_js_files = ["header-nav.js"]
+if (
+    "github_issues" in html_context
+    and html_context["github_issues"]
+    and not disable_feedback_button
+):
+    html_js_files.append("github_issue_links.js")
+html_js_files.extend(custom_html_js_files)
--- docs/index.rst
+++ docs/index.rst
@@ -1,7 +1,9 @@
 .. imagecraft documentation root file
 
-ImageCraft
+Imagecraft
 ==========
+
+Imagecraft is a tool for creating bootable Ubuntu images. It is a craft tool, which means it is based on the `Snapcraft framework`_.
 
 .. toctree::
    :maxdepth: 1
@@ -34,19 +36,26 @@ ImageCraft
       **Discussion and clarification** of key topics
 
 Project and community
-=====================
+---------------------
 
-Imagecraft is a member of the Canonical family. It's an open source project
-that warmly welcomes community projects, contributions, suggestions, fixes
-and constructive feedback.
+Imagecraft is a member of the Canonical family. It's an open source project that warmly welcomes community projects, contributions, suggestions, fixes and constructive feedback.
 
-* `Ubuntu Code of Conduct <https://ubuntu.com/community/code-of-conduct>`_.
-* `Canonical contributor licenses agreement
-  <https://ubuntu.com/legal/contributors>`_.
+Get started:
+  * Read our `Code of Conduct`_
 
-Indices and tables
-==================
+Discuss:
+  * Discourse: `Ubuntu Foundations`_
 
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
+..   * IRC: `Libera.Chat`_, the *#ubuntu-server* channel
+
+Contribute:
+  * `Contribution guidelines`_ on GitHub
+  * `Issue tracker`_ on GitHub
+
+.. Links:
+.. _Code of Conduct: https://ubuntu.com/community/ethos/code-of-conduct
+.. _Ubuntu Foundations: https://discourse.ubuntu.com/c/foundations/
+.. _Libera.Chat: https://libera.chat/
+.. _Contribution guidelines: https://github.com/canonical/imagecraft/blob/main/HACKING.rst
+.. _Issue tracker: https://github.com/canonical/imagecraft/issues/
+.. _Snapcraft framework: https://snapcraft.io/
