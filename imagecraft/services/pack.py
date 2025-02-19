b449659eb48ad706ab59c5f8817908d84317312f
Matt Culler <matt.culler@canonical.com>
2025-02-19 18:15:58 -0500

    feat: disk image creation

--- imagecraft/services/pack.py
+++ imagecraft/services/pack.py
@@ -1,4 +1,4 @@
-# Copyright 2023 Canonical Ltd.
+# Copyright 2023-2025 Canonical Ltd.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 3 as
@@ -14,17 +14,23 @@
 
 """Imagecraft Package service."""
 
-import pathlib
-import typing
+import os
+import tempfile
+from pathlib import Path
+from typing import TYPE_CHECKING, cast
 
 from craft_application import AppMetadata, PackageService, models
 from craft_application.models import BuildInfo
 from overrides import override  # type: ignore[reportUnknownVariableType]
 
-from imagecraft.models import Project
+from imagecraft.models import Project, get_partition_name
+from imagecraft.pack import diskutil, gptutil
 
-if typing.TYPE_CHECKING:
+if TYPE_CHECKING:
     from imagecraft.services import ImagecraftServiceFactory
+
+
+SECTOR_SIZE = 512
 
 
 class ImagecraftPackService(PackageService):
@@ -43,15 +49,63 @@ class ImagecraftPackService(PackageService):
         self._build_plan = build_plan
 
     @override
-    def pack(self, prime_dir: pathlib.Path, dest: pathlib.Path) -> list[pathlib.Path]:  # noqa: ARG002
+    def pack(self, prime_dir: Path, dest: Path) -> list[Path]:  # noqa: ARG002
         """Pack the image.
 
         :param prime_dir: Directory path to the prime directory.
         :param dest: Directory into which to write the package(s).
         :returns: A list of paths to created packages.
         """
+        # Pydantic has already validated that there is only a single volume before now
+        project = cast(Project, self._project)
+        if len(project.volumes) != 1:
+            raise AssertionError("This code can only handle one volume")
+        volume_name, volume = next(iter(project.volumes.items()))
+        disk_image_file = dest / (volume_name + os.extsep + "img")
 
-        return []
+        # Create empty image
+        gptutil.create_empty_gpt_image(
+            imagepath=disk_image_file,
+            sector_size=SECTOR_SIZE,
+            layout=volume,
+        )
+
+        # Create partition images with filesystems.  These are always recreated, but we
+        # may want to revisit that once this is solved:
+        # https://github.com/canonical/craft-parts/issues/665
+        project_dirs = self._services.lifecycle.project_info.dirs
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            for structure_item in volume.structure:
+                partition_name = get_partition_name(volume_name, structure_item)
+                partition_prime_dir = project_dirs.get_prime_dir(
+                    partition=partition_name
+                )
+                partition_img = (
+                    Path(tmp_dir) / f"{volume_name}.{structure_item.name}.img"
+                )
+                partition_size = diskutil.DiskSize(
+                    bytesize=structure_item.size,
+                    sector_size=SECTOR_SIZE,
+                )
+
+                diskutil.format_populate_partition(
+                    fstype=structure_item.filesystem,
+                    content_dir=partition_prime_dir,
+                    partitionpath=partition_img,
+                    disk_size=partition_size,
+                    label=structure_item.filesystem_label,
+                )
+                diskutil.inject_partition_into_image(
+                    partition=partition_img,
+                    imagepath=disk_image_file,
+                    sector_offset=gptutil.get_partition_sector_offset(
+                        disk_image_file,
+                        structure_item.name,
+                    ),
+                    disk_size=partition_size,
+                )
+        gptutil.verify_partition_tables(disk_image_file)
+        return [disk_image_file]
 
     @property
     def metadata(self) -> models.BaseMetadata:
@@ -60,7 +114,7 @@ class ImagecraftPackService(PackageService):
         return models.BaseMetadata()
 
     @override
-    def write_metadata(self, path: pathlib.Path) -> None:
+    def write_metadata(self, path: Path) -> None:
         """Write the project metadata to metadata.yaml in the given directory.
 
         :param path: The path to the prime directory.
