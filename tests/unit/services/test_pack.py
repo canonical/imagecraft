c86edbc030b75cb6fd85c10c687dae568ae67f82
Paul Mars <paul.mars@canonical.com>
2025-03-04 11:30:13 +0100

    test: use tmp_path in test_pack
    Signed-off-by: Paul Mars <paul.mars@canonical.com>

--- tests/unit/services/test_pack.py
+++ tests/unit/services/test_pack.py
@@ -1,4 +1,4 @@
-# Copyright 2023 Canonical Ltd.
+# Copyright 2023-2025 Canonical Ltd.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 3 as
@@ -12,19 +12,16 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from pathlib import Path
 
-
-def test_pack(pack_service, default_factory, mocker):
-    prime = "prime"
-    prime_dir = Path(prime)
-    dest_path = Path()
+def test_pack(tmp_path, pack_service, default_factory, mocker):
+    prime_dir = tmp_path / "prime"
+    dest_path = tmp_path / "dest"
 
     diskutil = mocker.patch("imagecraft.services.pack.diskutil", autospec=True)
     gptutil = mocker.patch("imagecraft.services.pack.gptutil", autospec=True)
 
     pack_service.setup()
-    assert pack_service.pack(prime_dir, dest=dest_path) == [Path("pc.img")]
+    assert pack_service.pack(prime_dir, dest=dest_path) == [dest_path / "pc.img"]
 
     assert gptutil.create_empty_gpt_image.called
     assert diskutil.inject_partition_into_image.called
