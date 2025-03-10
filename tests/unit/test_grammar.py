66c3a1690b269714ad41880e019d8d175265d12d
Paul Mars <paul.mars@canonical.com>
2025-03-10 14:54:05 +0100

    test: add simple process_volumes test
    Signed-off-by: Paul Mars <paul.mars@canonical.com>

---
+++ tests/unit/test_grammar.py
@@ -0,0 +1,107 @@
+# This file is part of imagecraft.
+#
+# Copyright 2025 Canonical Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import pytest
+import yaml
+from imagecraft.grammar import process_volumes
+
+
+@pytest.mark.parametrize(
+    ("volumes_yaml", "arch", "target_arch", "expected"),
+    [
+        (
+            """
+pc:
+  schema: gpt
+  structure:
+    - name: rootfs
+      type: 0FC63DAF-8483-4772-8E79-3D69D8477DE4
+      filesystem: ext4
+      filesystem-label: writable
+      role: system-data
+      size: 6GiB
+    """,
+            "amd64",
+            "amd64",
+            {
+                "pc": {
+                    "schema": "gpt",
+                    "structure": [
+                        {
+                            "name": "rootfs",
+                            "type": "0FC63DAF-8483-4772-8E79-3D69D8477DE4",
+                            "filesystem": "ext4",
+                            "filesystem-label": "writable",
+                            "role": "system-data",
+                            "size": "6GiB",
+                        }
+                    ],
+                },
+            },
+        ),
+        (
+            """
+pc:
+  schema: gpt
+  structure:
+    - to arm64:
+      - name: efi
+        role: system-boot
+        type: C12A7328-F81F-11D2-BA4B-00A0C93EC93B
+        filesystem: vfat
+        size: 500 MiB
+    - name: rootfs
+      type: 0FC63DAF-8483-4772-8E79-3D69D8477DE4
+      filesystem: ext4
+      filesystem-label: writable
+      role: system-data
+      size: 6GiB
+    """,
+            "amd64",
+            "arm64",
+            {
+                "pc": {
+                    "schema": "gpt",
+                    "structure": [
+                        {
+                            "name": "efi",
+                            "type": "C12A7328-F81F-11D2-BA4B-00A0C93EC93B",
+                            "filesystem": "vfat",
+                            "role": "system-boot",
+                            "size": "500 MiB",
+                        },
+                        {
+                            "name": "rootfs",
+                            "type": "0FC63DAF-8483-4772-8E79-3D69D8477DE4",
+                            "filesystem": "ext4",
+                            "filesystem-label": "writable",
+                            "role": "system-data",
+                            "size": "6GiB",
+                        },
+                    ],
+                },
+            },
+        ),
+    ],
+)
+def test_process_volumes(volumes_yaml, arch, target_arch, expected):
+    yaml_loaded = yaml.safe_load(volumes_yaml)
+    assert (
+        process_volumes(
+            volumes_yaml_data=yaml_loaded, arch=arch, target_arch=target_arch
+        )
+        == expected
+    )
