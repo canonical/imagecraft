f7d5a6ea9b3f064c5260210b9e8d2ad00fcbe6ce
jahn-junior <jj.coldiron@canonical.com>
2025-08-12 14:55:04 -0700

    docs: remove duplicate code

--- docs/conf.py
+++ docs/conf.py
@@ -66,15 +66,6 @@ html_js_files = [
     'js/bundle.js',
 ]
 
-# Sitemap configuration: https://sphinx-sitemap.readthedocs.io/
-html_baseurl = "https://canonical-imagecraft.readthedocs-hosted.com/"
-
-if "READTHEDOCS_VERSION" in os.environ:
-    version = os.environ["READTHEDOCS_VERSION"]
-    sitemap_url_scheme = "{version}{link}"
-else:
-    sitemap_url_scheme = "latest/{link}"
-
 extensions = [
     "canonical_sphinx",
     "notfound.extension",
