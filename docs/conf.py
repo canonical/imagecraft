451dddf296eb7e828fee0ef0938ba95b998e7332
Paul Mars <paul.mars@canonical.com>
2025-09-29 08:44:10 +0200

    style(docs): make linkcheck quieter and faster
    Signed-off-by: Paul Mars <paul.mars@canonical.com>

--- docs/conf.py
+++ docs/conf.py
@@ -160,15 +160,19 @@ exclude_patterns = [
     "common/craft-parts/reference/plugins/jlink_plugin.rst",
 ]
 
+# We have many links on sites that frequently respond with 503s to GitHub runners.
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-linkcheck_retries
+linkcheck_retries = 20
+linkcheck_anchors_ignore = ["#", ":"]
 linkcheck_ignore = [
-    "http://127.0.0.1:8000",
-    "https://apt-repo.com",
+    r"http://127.0.0.1:8000",
+    r"https://apt-repo.com",
     # Linkcheck is unable to properly handled matrix.to URLs containing # and :
     # See https://github.com/sphinx-doc/sphinx/issues/13620
-    "https://matrix.to",
-    # Ignore gnu.org URLs due to a mix of aggressive rate limiting and aggressive/buggy
-    # retry from linkcheck
-    "https://www.gnu.org"
+    r"https://matrix.to",
+    # Entire domains to ignore due to flakiness or issues
+    r"^https://www.gnu.org/",
+    r"^https://ubuntu.com",
 ]
 
 rst_epilog = """
