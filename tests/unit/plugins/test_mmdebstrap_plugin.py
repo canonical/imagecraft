bfa6be11dfe8dafed850071cfc8c3870a67a2ea7
smethnani <82812166+smethnani@users.noreply.github.com>
2026-03-16 10:40:39 +0000

    test: add unit tests

---
+++ tests/unit/plugins/test_mmdebstrap_plugin.py
@@ -0,0 +1,59 @@
+import pytest
+from craft_parts import PartInfo, ProjectInfo
+from craft_parts.parts import Part
+
+from imagecraft.plugins.mmdebstrap_plugin import (
+    MmdebstrapPlugin,
+    MmdebstrapPluginProperties,
+)
+
+
+@pytest.fixture
+def part_info(new_dir, request):
+    arch = getattr(request, "param", "amd64")
+    project_info = ProjectInfo(
+        application_name="test_mmdebstrap", cache_dir=new_dir, arch=arch
+    )
+    return PartInfo(project_info=project_info, part=Part("my-part", {}))
+
+
+@pytest.fixture
+def plugin(part_info):
+    properties = MmdebstrapPluginProperties.unmarshal({"mmdebstrap-suite": "noble"})
+    return MmdebstrapPlugin(properties=properties, part_info=part_info)
+
+
+MMDEBSTRAP_CMD = 'mmdebstrap --arch=$CRAFT_ARCH_BUILD_FOR --mode=auto --variant=minbase --format=dir noble "$CRAFT_PART_INSTALL"'
+
+
+def test_get_build_packages(plugin):
+    assert plugin.get_build_packages() == {"mmdebstrap"}
+
+
+def test_get_build_snaps(plugin):
+    assert plugin.get_build_snaps() == set()
+
+
+def test_get_build_commands(plugin):
+    cmd = plugin.get_build_commands()
+
+    assert cmd[0] == f"{MMDEBSTRAP_CMD} http://archive.ubuntu.com/ubuntu"
+    assert cmd[1] == 'rm -r "$CRAFT_PART_INSTALL"/dev/*'
+
+
+@pytest.mark.parametrize("part_info", ["arm64"], indirect=True)
+def test_get_build_commands_arm64(plugin):
+    cmd = plugin.get_build_commands()
+
+    assert cmd[0] == f"{MMDEBSTRAP_CMD} http://ports.ubuntu.com/ubuntu-ports"
+    assert cmd[1] == 'rm -r "$CRAFT_PART_INSTALL"/dev/*'
+
+
+def test_get_build_commands_include(part_info):
+    properties = MmdebstrapPluginProperties.unmarshal(
+        {"mmdebstrap-suite": "noble", "mmdebstrap-include": ["apt"]}
+    )
+    plugin = MmdebstrapPlugin(properties=properties, part_info=part_info)
+    cmd = plugin.get_build_commands()
+
+    assert "--include=apt" in cmd[0]
