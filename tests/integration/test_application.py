3a96fcb4d2b8ec48fbe3d61e19185726791989f6
smethnani <82812166+smethnani@users.noreply.github.com>
2026-03-23 10:48:58 +0000

    feat: enable override-overlay

--- tests/integration/test_application.py
+++ tests/integration/test_application.py
@@ -41,15 +41,15 @@ platforms:
 parts:
   rootfs:
     plugin: nil
-    overlay-script: |
-      mkdir $CRAFT_OVERLAY/{etc,bin,boot}
-      echo "test" > $CRAFT_OVERLAY/bin/a
-      echo "test" > $CRAFT_OVERLAY/etc/b
+    override-overlay: |
+      mkdir /{etc,bin,boot}
+      echo "test" > /bin/a
+      echo "test" > /etc/b
   bootloader:
     plugin: nil
     after: [rootfs]
-    overlay-script: |
-      echo "boot files" > $CRAFT_OVERLAY/boot/c
+    override-overlay: |
+      echo "boot files" > /boot/c
 
 filesystems:
   default:
