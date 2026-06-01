2f9b4ca0279d68bfdc44d13826c218ccc0f90759
smethnani <82812166+smethnani@users.noreply.github.com>
2026-06-01 07:52:26 +0100

    fix: tests

--- imagecraft/pack/chroot.py
+++ imagecraft/pack/chroot.py
@@ -83,7 +83,7 @@ class Mount:
             )
 
         logger.debug("[pid=%d] mount %r on chroot", pid, str(self._mountpoint))
-        os_utils.mount(self._src, str(self._mountpoint), *args)
+        os_utils.mount(self._src, self._mountpoint, *args)
 
     def umount(self, *, lazy: bool = False) -> None:
         """Umount the mountpoint."""
@@ -92,7 +92,7 @@ class Mount:
         if self._mountpoint and self._mountpoint.exists():
             logger.debug("[pid=%d] umount: %r", pid, str(self._mountpoint))
 
-            os_utils.mount(str(self._mountpoint), "--make-rprivate")
+            os_utils.mount(self._mountpoint, None, "--make-rprivate")
             args: list[str] = ["--recursive"]
 
             # Mount points under /dev may be in use and make the bind mount
