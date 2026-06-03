a223316cc20cb53519a07a4bd1c0405ced2a03af
smethnani <82812166+smethnani@users.noreply.github.com>
2026-06-03 16:56:10 +0100

    fix: remove sys/* proc/*

--- imagecraft/plugins/mmdebstrap_plugin.py
+++ imagecraft/plugins/mmdebstrap_plugin.py
@@ -103,6 +103,8 @@ class MmdebstrapPlugin(Plugin):
     def _get_cleanup_commands(self) -> list[str]:
         return [
             'for m in $(findmnt -rno TARGET | grep "^$CRAFT_PART_INSTALL/" | sort -r); do umount -lf "$m" 2>/dev/null || true; done',
+            'mountpoint -q "$CRAFT_PART_INSTALL"/sys || rm -rf "$CRAFT_PART_INSTALL"/sys/*',
+            'mountpoint -q "$CRAFT_PART_INSTALL"/proc || rm -rf "$CRAFT_PART_INSTALL"/proc/*',
             'rm -rf "$CRAFT_PART_INSTALL"/dev/*',
             'rm -rf "$CRAFT_PART_INSTALL"/etc/apt/sources.list.d/*',
             'rm -f "$CRAFT_PART_INSTALL"/etc/apt/sources.list',
