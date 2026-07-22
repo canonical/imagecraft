c5b8dda46a38db089ef16a1cb9664e765cf2a74e
smethnani <82812166+smethnani@users.noreply.github.com>
2026-07-22 15:31:17 +0100

    test: add core boot spread test (#375)

---
+++ tests/spread/boot/core/sentinel/bin/sentinel.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$(uname -m)" = "x86_64" ]; then
+  SERIAL_CONSOLE="/dev/ttyS0"
+else
+  SERIAL_CONSOLE="/dev/ttyAMA0"
+fi
+echo "HELLO FROM SENTINEL SNAP" > $SERIAL_CONSOLE
+
+sleep 5
+dbus-send --system --print-reply \
+        --dest=org.freedesktop.login1 /org/freedesktop/login1 \
+        "org.freedesktop.login1.Manager.PowerOff" boolean:true
